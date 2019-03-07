cf488a0a44a860170b049395255fd9bebc31eeaa
JulieNobody <julie.nobody@gmail.com>
2019-03-07 18:39:01 +0100

    4 - ajout fichier score et affichage

--- jeucolas.sh
+++ jeucolas.sh
@@ -14,6 +14,7 @@ number=$[($RANDOM % ($[$max - $min] + 1)) + $min]
 #variable nombre d'essai
 essais=1
 
+#affichage de la solution
 echo " "
 echo "     $number"
 echo " "
@@ -28,10 +29,11 @@ echo "~~ Vous allez devoir deviner ce nombre en un minimum de tentative ~~"
 echo "~~ Pour chaque tentative, je vous dirais si vous êtes trop haut ou trop bas ~~"
 echo " "
 echo " "
+#demande de saisie pour le 1er essais
 echo " ~ essai numero $essais ~ "
 read -p "à vous de jouer : " reponse
 
-
+#Test des variables saisies
 while [ $reponse -ne $number ]
 do
 			if [ $reponse -lt $number ]
@@ -50,11 +52,20 @@ do
 		fi
 done
 
+#si bonne réponse
 if [ $reponse -eq $number ]
 then
 	echo " "
 	echo "*** Félicitation ! vous avez gagné ! ***"
 	echo "*** Votre score : $essais essais ***"
+	echo " "
+#saisie du nom du joueur
+	read -p "Quel est votre nom ? " joueur
+#affichage des scores
+	echo "$essais essais - $joueur" >> scores.txt
+	echo " "
+	echo "~~~~~ Scores ~~~~~"
+	sort -n scores.txt
 	echo " "
 fi
 
